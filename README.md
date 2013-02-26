0367facbc13bfd4f7fac479c72f00ca0ebe2bc0c
Eric D. Schabell <eric@schabell.org>
2013-02-26 20:20:11 +0100

    Added initial setup for fuse a-mq demo based on Charles websocket demo.

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-jboss-a-mq-websockets-demo
-==========================
+# Example showing How to use WebSocket HTML 5 with JBoss A-MQ and Camel
 
-JBoss A-MQ demo showing How to use HTML5 WebSockets.
+## JBoss A-MQ
+
+1) Start JBoss a-mq using the shell or .bat script under bin directory
+   bin/a-mq
+
+              _ ____                                __  __  ____
+             | |  _ \                    /\        |  \/  |/ __ \
+             | | |_) | ___  ___ ___     /  \ ______| \  / | |  | |
+         _   | |  _ < / _ \/ __/ __|   / /\ \______| |\/| | |  | |
+        | |__| | |_) | (_) \__ \__ \  / ____ \     | |  | | |__| |
+         \____/|____/ \___/|___/___/ /_/    \_\    |_|  |_|\___\_\
+
+         JBoss A-MQ (6.0.0.redhat-009)
+         http://fusesource.com/products/fuse-mq-enterprise/
+
+       Hit '<tab>' for a list of available commands
+       and '[cmd] --help' for help on a specific command.
+       Hit '<ctrl-d>' or 'osgi:shutdown' to shutdown JBoss A-MQ.
+
+       JBossA-MQ:karaf@root>
+
+2) When the JBoss-AMQ console appears, install the activemq-websocket war file. This war file contains the
+   web project and stomp javascript clients used to open communication between the web browser and websocket
+   server running in JBoss A-MQ.
+
+    JBossA-MQ:karaf@root>install -s war:mvn:org.fusesource.examples.activemq.websocket/web/1.0/war\?Webapp-Context=activemq-websocket
+
+3)  Start Feeder application, which will populate randomly data (stock prices) and publish them in a topic which is the 
+    topic used by websocket to expose the date to the web browser. You will find this in the 'support' directory.
+
+    start_feeder.sh
+
+4) Open your web browser and point to the following URL.
+
+    http://localhost:8181/activemq-websocket/stocks-activemq.html
+
+5) Click on connect button.
+
+   Remark : The login is 'guest':'password'
+
+   Consult stock prices !
+
+## Camel
+
+1) Start Apache Camel Routes with or without SSL, using file found in 'suppport' directory.
+
+   start_camel_nossl.sh  or  start_camel_ssl.sh
+
+2) Start Feeder application
+
+   start_feeder.sh
+
+3) Verify stock and news websockets in your browser.
+
+    http://localhost:9090/stocks-camel.html
+    http://localhost:9090/news-camel.html
+
+    or 
+
+    https://localhost:8443/news-camel-wss.html
+
+    and click on connect button.
+
---
+++ init.sh
@@ -0,0 +1,97 @@
+#!/bin/sh 
+DEMO="JBoss A-MQ Websocket Demo"
+VERSION=6.0.0
+AMQ=jboss-a-mq-6.0.0.redhat-009
+DEMO_HOME=./target
+AMQ_HOME=$DEMO_HOME/$AMQ
+SERVER_DIR=$AMQ_HOME/
+SERVER_CONF=$AMQ_HOME/etc
+SRC_DIR=./installs
+PRJ_DIR=./projects/websocket-activemq-camel
+
+
+echo
+echo "Setting up the Red Hat ${DEMO} environment..."
+echo
+
+# double check for maven.
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [[ -r $SRC_DIR/$AMQ.tar.gz || -L $SRC_DIR/$AMQ.tar.gz ]]; then
+		echo JBoss A-MQ sources are present...
+		echo
+else
+		echo Need to download $AMQ.tar.gz package from the Customer Support Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+
+# Create the target directory if it does not already exist.
+if [ ! -x $DEMO_HOME ]; then
+		echo "  - creating the demo home directory..."
+		echo
+		mkdir $DEMO_HOME
+else
+		echo "  - detected demo home directory, moving on..."
+		echo
+fi
+
+
+# Move the old JBoss instance, if it exists, to the OLD position.
+if [ -x $AMQ_HOME ]; then
+		echo "  - existing JBoss A-MQ detected..."
+		echo
+		echo "  - moving existing JBoss A-MQ aside..."
+		echo
+		rm -rf $AMQ_HOME.OLD
+		mv $AMQ_HOME $AMQ_HOME.OLD
+
+		# Unzip the JBoss A-MQ instance.
+		echo Unpacking JBoss A-MQ $VERSION
+		echo
+		tar -zxf $SRC_DIR/$AMQ.tar.gz 
+		mv $AMQ $DEMO_HOME
+else
+		# Unzip the JBoss A-MQ instance.
+		echo Unpacking new JBoss A-MQ...
+		echo
+		tar -zxf $SRC_DIR/$AMQ.tar.gz
+	  mv $AMQ	$DEMO_HOME
+fi
+
+
+echo "  - enabling demo accounts logins in users.properties file..."
+echo
+cp support/users.properties $SERVER_CONF
+
+echo "  - copying updated JBoss A-MQ configuration file fuseamq-websocket.xml from project..."
+echo
+cp projects/websocket-activemq-camel/feeder/src/main/config/activemq-websocket.xml $SERVER_CONF/activemq.xml
+
+echo "  - making sure 'a-mq' for server is executable..."
+echo
+chmod u+x $AMQ_HOME/bin/a-mq
+
+echo Now going to build the Feeder project.
+echo
+cd $PRJ_DIR/feeder
+mvn clean install -DskipTests
+
+echo
+echo Now going to build the Camel project.
+echo
+cd ../camel
+mvn clean install -DskipTests
+
+echo
+echo To get started see the README.md file:
+echo
+cd ../../..
+cat README.md
+
+echo Red Hat $DEMO $VERSION Setup Completed.
+echo
+
